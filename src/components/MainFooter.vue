27c50894eba4b97feeabc974da6b491ffff5b649
Victor Salinas Sandoval <sandovaldev21@gmail.com>
2025-04-18 11:15:14 -0600

    Adding Footer.

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="text-center py-4 text-sm" :style="{ color: 'var(--color-text)' }">
+      &copy; Sandovaldev 2025
+    </footer>
+  </template>
+  
+  <script setup>
+ //No se necesita nada aquí
+  </script>
+  
+  <style scoped>
+  footer {
+    background: transparent;
+  }
+  </style>
+  
